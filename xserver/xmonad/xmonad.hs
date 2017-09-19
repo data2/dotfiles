35424e7ecdbaef5f567c42e4c5a6ceb64076f494
Jonas Fietz <info@jonasfietz.de>
2017-09-19 12:23:04 +0200

    updates vim, gdb, xmonad

--- xserver/xmonad/xmonad.hs
+++ xserver/xmonad/xmonad.hs
@@ -210,11 +210,13 @@ myKeyBindings =
     , ((myModMask, xK_p), spawn "synapse")
     , ((myModMask .|. mod1Mask, xK_space), spawn "synapse")
     , ((myModMask, xK_u), focusUrgent)
-    , ((0, 0x1008FF12), spawn "amixer -q set Master toggle")
+    , ((0, 0x1008FF12), spawn "amixer -q set Master toggle;  amixer -q set Speaker toggle;")
     , ((0, 0x1008FF11), spawn "amixer -q set Master 5%-")
     , ((0, 0x1008FF13), spawn "amixer -q set Master 5%+")
     , ((noModMask, xF86XK_MonBrightnessUp), spawn "xbacklight +9")
     , ((noModMask, xF86XK_MonBrightnessDown), spawn "xbacklight -9")
+    ,((myModMask .|. shiftMask, xK_l), spawn "gnome-screensaver-command -l")
+
 
   ]
 
